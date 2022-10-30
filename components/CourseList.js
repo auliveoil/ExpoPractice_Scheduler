d02026738bff899574efab729829279df7df9de8
auliveoil <shannonhan2023@u.northwestern.edu>
2022-10-30 10:10:35 -0500

    Modularize the components

---
+++ components/CourseList.js
@@ -0,0 +1,19 @@
+import Course from './Course';
+import { StyleSheet, ScrollView, View} from 'react-native';
+const CourseList = ({ courses }) => (
+    <ScrollView>
+      <View style={styles.courseList}>
+        { courses.map(course => <Course key={course.id} course={ course } />) }
+      </View>
+    </ScrollView>
+  );
+const styles = StyleSheet.create({
+    courseList: {
+      flex: 1,
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+    },
+  });
+  export default CourseList;
